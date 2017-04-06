540db66c7241f33c62de652f444d6ba708bddfe2
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-04-06 16:48:40 +0100

    Update boardOperations.c

--- boardOperations.c
+++ boardOperations.c
@@ -323,3 +323,36 @@ void wizard(struct player *Current_Player)
 	Current_Player->dexterity = (rand() % 101);  //Pick a random number from 0 and 100
 	Current_Player->life_points = 100;  //Set life points to 100
 }
+void NearAttack(struct player *AttackerPlayer, struct player *AttackedPlayer)
+{
+	if(AttackedPlayer->strength <=70)
+	{
+		AttackedPlayer->life_points = AttackedPlayer->life_points - (0.5 * AttackedPlayer->strength);
+		printf("1. Attacked Player Life points = %d", AttackedPlayer->life_points);
+	}
+	else if(AttackedPlayer->strength > 70)
+	{
+		AttackerPlayer->life_points = AttackerPlayer->life_points - (0.3 * AttackedPlayer->strength);
+		printf("2. Attcker Player Life Points = %d", AttackerPlayer->life_points);
+	}
+}
+
+void DistantAttack(struct player *AttackerPlayer, struct player *AttackedPlayer)
+{
+	if(AttackedPlayer->dexterity >= AttackerPlayer->dexterity)
+	{
+		AttackedPlayer->life_points = AttackedPlayer->life_points;
+	}
+	else if(AttackerPlayer->dexterity > AttackedPlayer->dexterity)
+	{
+		AttackedPlayer->life_points AttackedPlayer->life_points - (0.3 * AttackerPlayer->strength);
+	}
+}
+
+void MagicAttack(struct player *AttackerPlayer, struct player *AttackedPlayer)
+{
+	if(AttackerPlayer->smartness + AttackerPlayer->magic_skills > 150)
+	{
+		AttackedPlayer->life_points = AttackedPlayer->life_points - ((0.5 * AttackerPlayer->magic_skills) + (0.2 * AttackerPlayer->smartness));
+	}
+}
