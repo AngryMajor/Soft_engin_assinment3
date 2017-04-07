fe34373d4f3c8296f373516c1f967d9d5c43db5b
AngryMajor <peterfmajor@gmail.com>
2017-04-07 12:40:32 +0100

    Update userInteraction.c

--- userInteraction.c
+++ userInteraction.c
@@ -47,3 +47,46 @@ void getDesiredElement(int boardSize, int * row, int * col){
 				printf("Error: Incorrect column dimension\n");
 		} while(*col < 0 && *col >= boardSize);
 }
+
+/*
+funciton that gets the players action off the user
+takes a player as input
+returns an int of what they want to do
+0=quit 1=move 2=near attack 3=far attack 4=magic attack
+*/
+int getPlayerAction(struct player Player){
+	
+	int option;//stor input
+	//check the curent player can do magic
+	int canMagic = 0;
+	if(Player.magic_skills + Player.smartness > 150) canMagic = 1;
+	
+	//get action from user
+	printf("What would you like to do?\n0: quit\n1: move\n2: attack");
+	scanf("%d",option);
+	
+	//check option is valid, make the user go again if its not
+	if(option<0 ||option >2){
+		printf("invalid input");
+		return getPlayerAction(Player);
+	}
+	
+	if(option == 2){//if player wants to attack
+		
+		printf("please input what kind of attack youd like to do?\n0:near\n1:far\n");
+		if(canMagic) printf("2:magic\n");
+		int option2;
+		scanf("%d",option2);
+		
+		//check option is valid
+		if(option2<0 ||option2 >(1+canMagic)){
+			printf("invalid input");
+			return getPlayerAction(Player);
+		}
+		
+		option += option2;
+	}//end of attack option
+	
+	return option; 
+	
+}//end of get player action 
