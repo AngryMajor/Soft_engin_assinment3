09f95e503b522216d9c6c8282d9560321d676841
AngryMajor <peterfmajor@gmail.com>
2017-04-07 16:02:48 +0100

    Update userInteraction.c

--- userInteraction.c
+++ userInteraction.c
@@ -9,15 +9,6 @@
 #include <stdio.h>
 #include "crossfireOperations.h"
 
-//This function returns the board size provided as input by the user
-int getBoardSize(){
-	int boardSize;
-	printf("Please enter the required size of the board\n");
-	//asks the user to enter the desired board size
-	scanf("%d", &boardSize);
-	return boardSize;
-}
-
 //THis function takes as input the size of the board and the pointers
 // to the integers representing the row and the column of the slot that
 // the user want to retrieve
