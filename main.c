86b13f9c61b5a097eb7b27a9b92db3ca39192afd
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-04-08 18:50:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,61 +3,41 @@
 
 int main()
 {
-	//creat the board 
-	createBoard(); 
+
+	int row, columm;
+	createBoard(); //Creates the board
 	
-	//creat the players
-	int choice;//Number to be entered by user
-	printf("Please input how many players are playing the game: ");  //Prompt user to enter a number
-	scanf("%d", &choice);  //Reads an integer used to represent the number of players playing the game
-	
-	//check there are a valid number of players
-	if(choice<2){
-		printf("you must have at least 2 players\n\n");
-		return main();
-	}	
-	if(choice>6){
-		printf("you may only have up to 6 players\n\n");
-		return main();
-	}
-	
-	//creat player data
-	struct player players[choice];  
-	creatPlayers(players,choice);
-	
+	struct player players[numberofplayers];
 	
 	
 	PlacePlayer(players[0]);
 	
 	//round loop
-	int loop = 1;
-	while(loop){
-		for(int i=0;i<choice;i++)
+		for(int i=0;i<numberofplayers;i++)
 		{ //Start of player turn loop
-			if(!players[i].dead){
-				printf("\nit is player %d's turn\n\n",i);
-				int action = getPlayerAction(players[0]);
-				switch (action){
-					case 0: //Quit
-						players[i].dead = 1;
-						break;
-					case 1: //Move
-						break;
-					case 2: //Near Attack
-						
-						break;
-					case 3: //Distant Attack
-						
-						break;
-					case 4: //Magic Attack
-						choosePlayer_MagAtt(choice,players[i], players);
-						break;
+			int action = getPlayerAction(players[0]);
+			switch (action){
+				case 0: //Quit
+					break;
+				case 1: //Move
+					break;
+				case 2: //Near Attack
 					
-				} //End of switch action
-			}
+					break;
+				case 3: //Distant Attack
+					
+					break;
+				case 4: //Magic Attack
+					choosePlayer_MagAtt(numberofplayers,players[i], players);
+					break;
+				
+			} //End of switch action
 		} //End of loop for player's turn
 		//Check if the game terminates
-		loop = AllPlayersNotDead(players, choice);
-	}
+	
+	int choice;  //Number to be entered by user
+	
+
+
 return 0;
 }
