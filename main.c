a528d454577241c82dcdad8bd9962c63a3176453
AngryMajor <peterfmajor@gmail.com>
2017-04-09 17:33:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,61 @@
 #include "crossfireOperations.h"
-#define numberofplayers 6
 
 int main()
 {
 
 	createBoard(); //Creates the board
 	
+	
+	int numberofplayers;
+	printf("how many players would like to play?");
+	scanf("%d",&numberofplayers);
+	if(numberofplayers<2){
+		printf("must be at least 2 players");
+		return main();
+	}else if(numberofplayers>6){
+		printf("there can not be more then 6 players");
+		return main();
+	}
+	
 	struct player players[numberofplayers];
 	
+	creatPlayers(players,numberofplayers);
 	
-	PlacePlayer(players[0]);
+	for(int i=0;i<numberofplayers;i++){
+		PlacePlayer(players[i]);
+	}
 	
-	//round loop
+	int turnFail;//if turn fail = 1 the player can repeat their turn
+	int loop = 1;
+	while(loop){
 		for(int i=0;i<numberofplayers;i++)
-		{ //Start of player turn loop
-			int action = getPlayerAction(players[0]);
-			switch (action){
-				case 0: //Quit
-					break;
-				case 1: //Move
-					break;
-				case 2: //Near Attack
-					
-					break;
-				case 3: //Distant Attack
-					
-					break;
-				case 4: //Magic Attack
-					choosePlayer_MagAtt(numberofplayers,players[i], players);
-					break;
+			{ //Start of player turn loop
+			do{
+				printf("\nplayer %d, its your turn\n\n",i);
+			
+				int action = getPlayerAction(players[0]);
+				switch (action){
+					case 0: //Quit
+						players[i].dead = 1;
+						break;
+					case 1: //Move
+						turnFail = move(&players[i]);
+						break;
+					case 2: //Near Attack
+						turnFail = choosePlayer_NearAtt(&players[i],players, numberofplayers);
+						break;
+					case 3: //Distant Attack
+						turnFail = choosePlayer_DistantAtt(players[i],players);
+						break;
+					case 4: //Magic Attack
+						turnFail = choosePlayer_MagAtt(numberofplayers,players[i],players);
+						break;
 				
-			} //End of switch action
+				} //End of switch action
+			}while(turnFail);
 		} //End of loop for player's turn
-		//Check if the game terminates
-	
-	int choice;  //Number to be entered by user
-	
-
+		loop = AllPlayersNotDead(players,numberofplayers);
+	}//Check if the game terminates
 
 return 0;
 }
